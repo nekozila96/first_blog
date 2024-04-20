f1ec7bf6884550512d208b44f707fa5d012140cd
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-20 22:35:37 +0700

    Update Python_Setup.sh

--- scripts/Python_Setup.sh
+++ scripts/Python_Setup.sh
@@ -7,7 +7,7 @@ sudo yum -y groupinstall development
 sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel sqlite-devel python-pip libsqlite3-dev
 sudo yum -y install policycoreutils policycoreutils-python selinux-policy
 #Install Python 3.X.X
-sudo cd /usr/src
+cd /usr/src
 sudo wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
 sudo tar xzf Python-3.7.3.tgz
 sudo cd Python-3.7.3
