784140cb9849c1ba922c703655f65139e948805d
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-20 21:21:11 +0700

    Update Python_Env.sh

--- Python_Env.sh
+++ Python_Env.sh
@@ -9,12 +9,11 @@ fi
 echo $PWD
 
 source my_env/bin/activate
-<<<<<<< HEAD:Python_Env.sh
 
-=======
+
 cd /var/lib/jenkins/workspace/Django/scripts
 python -m pip install requirements.txt
->>>>>>> 72539bbac59354838833ed14f5f7444d19140e90:scripts/Python_Env.sh
+
 
 echo "Python Env finishes"
 
