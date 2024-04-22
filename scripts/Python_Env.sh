994bc49610795a8d4e6f74f8591555335d33df15
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-22 15:03:53 +0700

    Update Python_Env.sh

--- scripts/Python_Env.sh
+++ scripts/Python_Env.sh
@@ -3,6 +3,11 @@
 echo $PWD
 cd /var/lib/jenkins/workspace/Django
 python --version
+if [ -d "my_env" ]; then
+    echo "Thư mục tồn tại."
+else
+    python -m venv my_env
+fi
 source my_env/bin/activate
 python -m pip install --upgrade pip
 python -m pip install django==2.0
