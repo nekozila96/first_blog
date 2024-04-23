c457dbb1f20c66d1f869c45d10c19dd3067ad79a
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-23 21:42:14 +0700

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,17 +1,17 @@
 
 #!bin/bash
 usermod -a -G root jenkins
-sudo cp -rf nginx.repo /etc/yum.repos.d
-sudo yum -y install nginx
-sudo cp -rf gunicorn.service /etc/systemd/system
+cp -rf nginx.repo /etc/yum.repos.d
+yum -y install nginx
+cp -rf gunicorn.service /etc/systemd/system
 usermod -a -G nginx root
 
 echo "$USER"
 echo "$PWD"
-sudo systemctl start gunicorn.service
-sudo systemctl daemon-reload
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
+systemctl start gunicorn.service
+systemctl daemon-reload
+systemctl start gunicorn
+systemctl enable gunicorn
 
 echo "Gunicorn has been started"
 
