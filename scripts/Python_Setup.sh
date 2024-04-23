16751d2c6e731cc0900310be501fc0ba955c8e51
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-23 22:29:26 +0700

    Update Python_Setup.sh

--- scripts/Python_Setup.sh
+++ scripts/Python_Setup.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 sudo apt-get update
 sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
