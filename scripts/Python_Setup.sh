ce1ff1736b80af9ebef98f45c378439e65dbfda5
nekozila96 <theanhnguyenk102@gmail.com>
2024-04-23 22:22:46 +0700

    Update Python_Setup

--- scripts/Python_Setup.sh
+++ scripts/Python_Setup.sh
@@ -1,28 +1,13 @@
 #!bin/bash
-python --version
+cd /var/lib/jenkins
+sudo apt-get update
+sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-#Install and Update Yum
-sudo yum -y update
-sudo yum -y install yum-utils
-sudo yum -y groupinstall development 
-sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel sqlite-devel python-pip libsqlite3-dev
-sudo yum -y install policycoreutils policycoreutils-python selinux-policy
-sudo cp -rf nginx.repo /etc/yum.repos.d
-sudo yum -y install nginx
-#Install Python 3.X.X
-cd /usr/src
-sudo wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
-sudo tar xzf Python-3.7.3.tgz
-cd Python-3.7.3
-./configure --enable-loadable-sqlite-extensions && make && sudo make install
-which python3.7
-
-
-python --version
-sudo yum install -y java-11-openjdk-devel
-curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
-sudo yum install -y jenkins--nogpgcheck
-sudo systemctl start jenkins
-sudo systemctl enable jenkins
-sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp
-sudo firewall-cmd --reload
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+exec "$SHELL"
+pyenv --version
+pyenv install 3.6.5
+pyenv global 3.6.5
+pyenv global
