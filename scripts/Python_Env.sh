26a28e8d7a29f24d57c6ed8cec53af8e4d89189a
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-23 21:53:07 +0700

    Update Python_Env.sh

--- scripts/Python_Env.sh
+++ scripts/Python_Env.sh
@@ -2,17 +2,17 @@
 cd /var/lib/jenkins/workspace/Django
 python --version
 
-python -m venv my_env
+python3 -m venv my_env
 source ./my_env/bin/activate
-pip install --upgrade pip
-python install django==2.0 
-python install gunicorn 
+pip3 install --upgrade pip
+python3 install django==2.0 
+python3 install gunicorn 
 
 
 echo "Python Env finishes"
 
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 echo "Migrations done"
 deactivate
