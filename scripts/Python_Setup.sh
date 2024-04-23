90b263b935c27d502ac5ca0d709014fa62507c69
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-23 22:33:16 +0700

    Update Python_Setup.sh

--- scripts/Python_Setup.sh
+++ scripts/Python_Setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo yum update
+sudo yum update -y
 sudo yum -y install git gcc zlib-devel bzip2-devel readline-devel sqlite-devel openssl-devel
-
+git clone https://github.com/pyenv/pyenv.git $HOME/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
-exec "$SHELL"
+source $HOME/.bashrc
 pyenv --version
 pyenv install 3.6.5
 pyenv global 3.6.5
