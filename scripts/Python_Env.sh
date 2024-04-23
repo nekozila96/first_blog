8aeb9f2fe12859264be41e6e64b98f7a8f5c9888
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-23 22:58:21 +0700

    Update Python_Env.sh

--- scripts/Python_Env.sh
+++ scripts/Python_Env.sh
@@ -31,3 +31,22 @@ python3 manage.py migrate
 
 echo "Migrations done"
 deactivate
+
+python --version
+python -m gunicorn --version
+
+sudo cp -rf nginx.repo /etc/yum.repos.d
+sudo yum -y install nginx
+sudo cp -rf gunicorn.service /etc/systemd/system
+
+
+echo "$USER"
+echo "$PWD"
+sudo systemctl start gunicorn.service
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn has been started"
+
+sudo systemctl status gunicorn
