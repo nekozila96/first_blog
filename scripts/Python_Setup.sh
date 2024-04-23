feac11a052d98a5f61828bce84d8d27b576f1b69
nekozila96 <theanhnguyenk102@gmail.com>
2024-04-23 22:25:27 +0700

    Update Python_Setup

--- scripts/Python_Setup.sh
+++ scripts/Python_Setup.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-cd /var/lib/jenkins
+
 sudo apt-get update
 sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
