3eaa8c2ef9afc85d94db61536bc63bf43dfd7fc1
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-24 14:45:36 +0700

    Update Python_Env.sh

--- scripts/Python_Env.sh
+++ scripts/Python_Env.sh
@@ -54,5 +54,22 @@ sudo systemctl enable gunicorn
 sudo systemctl status gunicorn
 echo "Gunicorn has been started"
 
+sudo cp -rf testdjango.conf /etc/nginx/conf.d
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --permanent --zone=public --add-service=https
+sudo firewall-cmd --reload
+
+sudo sed -i 's/PrivateTmp=True/PrivateTmp=False/g' /lib/systemd/system/nginx.service
+setsebool -P httpd_can_network_connect 1
+setsebool -P httpd_can_network_relay 1
+sudo nginx -t
+sudo sed -i 's/^SELINUX=.*/SELINUX=permissive/' /etc/selinux/config
+
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo systemctl status nginx
+
+echo "Gunicorn has been started"
+
 
 deactivate
