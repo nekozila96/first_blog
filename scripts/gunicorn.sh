d33c4a375e7ceb0922941ddf19b16d387f4ba024
nekozila96 <154298967+nekozila96@users.noreply.github.com>
2024-04-24 14:20:04 +0700

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -10,8 +10,8 @@ sudo cp -rf gunicorn.service /etc/systemd/system
 
 echo "$USER"
 echo "$PWD"
-sudo systemctl start gunicorn.service
 sudo systemctl daemon-reload
+sudo systemctl start gunicorn.service
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 
